1de0525f2e7b0fe93df6a591e9d324ae6e7d7513
eloveday <emilyloveday91@gmail.com>
2021-11-21 15:21:27 +0000

    Added source code link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <WeatherForm />
+      <p>
+        Source code at{" "}
+        <a href="https://github.com/eloveday/react-weather-app">Github</a>
+      </p>
     </div>
   );
 }
