d77272123e1ffb9a1bb32c4df8d7f36f3c7228fa
eloveday <emilyloveday91@gmail.com>
2021-11-25 20:41:15 +0000

    Adjusted grid layout and spacing

--- src/WeatherResults.js
+++ src/WeatherResults.js
@@ -1,24 +1,29 @@
 import React from "react";
+import "./WeatherResults.css";
 
 export default function WeatherResults() {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col">
+      <div className="row mt-2">
+        <div className="col text-center">
           <h2>New York</h2>
         </div>
+        <div className="col text-center">Friday 17:00</div>
       </div>
       <div className="row">
-        <div className="col-6">
+        <div className="col text-center">
           <ul>
-            <li>Temperature: 10</li>
             <li>Cloudy</li>
             <li>Humidity: 75%</li>
             <li>Wind: 75 kph</li>
           </ul>
         </div>
-        <div className="col-6">
-          <img src="http://openweathermap.org/img/wn/01d.png" />
+        <div className="col text-center temperature">
+          10C{" "}
+          <img
+            src="http://openweathermap.org/img/wn/01d.png"
+            alt="weather-icon"
+          />
         </div>
       </div>
     </div>
